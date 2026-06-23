b7ddfd194a2f092f0beb9089fd9b92a7653ae1dc
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-06-23 16:50:24 +0530

    .

---
+++ Introduction/script.js
@@ -0,0 +1,17 @@
+// function and javascript
+// arrays and object 
+// function return
+// async and coding
+
+var arr = [1,2,3,4];
+// arr.forEach(function(val){
+//     console.log(val + " Hello");
+// });
+
+
+// map 
+
+var ans = arr.map(function(val){
+    return val+12;
+})
+console.log(ans);
