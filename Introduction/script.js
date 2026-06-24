8c5de7ab56c4203dc814505a327073cd2bf93643
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-06-24 17:40:08 +0530

    something new

--- Introduction/script.js
+++ Introduction/script.js
@@ -1,9 +1,9 @@
 // function and javascript
 // arrays and object 
 // function return
-// async and coding
+// async js coding
 
-var arr = [1,2,3,4];
+// var arr = [1,2,3,4];
 // arr.forEach(function(val){
 //     console.log(val + " Hello");
 // });
@@ -11,7 +11,24 @@ var arr = [1,2,3,4];
 
 // map 
 
-var ans = arr.map(function(val){
-    return val+12;
-})
-console.log(ans);
+// var ans = arr.map(function(val){
+//     return val+12;
+// })
+// console.log(ans);
+
+
+// objects
+
+// let obj = {
+//     name : "Aditya",
+//     age : 20
+// };
+async function abcd() {
+    var blob = await fetch(`https://randomuser.me/api/`);
+    var res = await blob.json();
+    
+    console.log(res);
+    
+}
+
+abcd();
