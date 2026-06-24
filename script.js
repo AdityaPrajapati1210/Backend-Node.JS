3772f310cc83db9a415a0ca9e9df216d673ccf38
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-06-24 18:16:53 +0530

    .

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const fs = require('node:fs');
 
-fs.writeFile("hey.txt","Its very to understand the Node js",function(err){
-    if(err) console.error(err);
-    else console.log("done");
-});
+
+function create(){
+    fs.writeFile("hey.txt","Its very to understand the Node js",function(err){
+        if(err) console.error(err);
+        else console.log("done");
+    });
+
+}
+
+function rename(){
+    fs.rename("myFile.cpp","myFile.txt",function(err){
+        if(err) console.error(err);
+        else console.log("successfull");
+    });
+
+}
+
+rename();
