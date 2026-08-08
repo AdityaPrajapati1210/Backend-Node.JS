d7392a5d53c56980665c00077b0049d8c9b16bcf
AdityaPrajapati1210 <188994428+AdityaPrajapati1210@users.noreply.github.com>
2026-08-08 14:07:56 +0530

    components, hooks, efficency

---
+++ CurrencyConverter/hooks/useCurrency.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+
+function useCurrency(currency) {
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://open.er-api.com/v6/latest/${currency}`)
+            .then((res) => res.json())
+            .then((res) => setData(res.rates));
+    }, [currency])
+    console.log(data);
+    return data;
+}
+
+export default useCurrency;
