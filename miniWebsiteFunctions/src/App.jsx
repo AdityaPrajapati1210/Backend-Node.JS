8b5d015d5d48c786154321d884a7ffb8b963d036
AdityaPrajapati1210 <188994428+AdityaPrajapati1210@users.noreply.github.com>
2026-08-12 18:02:06 +0530

    react-router-dom

---
+++ miniWebsiteFunctions/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="bg-red-400">meow</h1>
+      <p className=''></p>
+    </>
+  )
+}
+
+export default App
